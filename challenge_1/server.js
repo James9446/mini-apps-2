6a995ce328f8d3a3e0cde82fa89e5e4466392598
James Reynolds <jameshreynolds@gmail.com>
2018-04-04 12:35:51 -0700

    Initial environment setup

--- challenge_1/server.js
+++ challenge_1/server.js
@@ -1,2 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
 
+app.use(express.static(path.join(__dirname, 'dist')));
+// app.use(express.static('dist'))
+// app.use('/static', express.static(path.join(__dirname, 'dist')))
+ 
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+
+
+app.listen(3000, ()=>{
+    console.log('listening on port 3000')
+})
 
---
+++ challenge_1/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// let date = new Date();
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>Setup yup yup oh yeah is successful</div>
+  }
+}
+
+export default App;
+
+module.hot.accept();
